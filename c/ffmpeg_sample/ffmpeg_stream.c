07bd93325308e4e1b6bed2cfe43c3e7fbea961c6
txgcwm <txgcwm@163.com>
2016-11-19 15:13:09 +0800

    compile

--- c/ffmpeg_sample/ffmpeg_stream.c
+++ c/ffmpeg_sample/ffmpeg_stream.c
@@ -71,28 +71,30 @@ int main(int argc, char *argv[])
 		ret = AVERROR_UNKNOWN;
 		goto end;
 	}
+
 	ofmt = ofmt_ctx->oformat;
 	for (i = 0; i < ifmt_ctx->nb_streams; i++) {
 		//根据输入流创建输出流（Create output AVStream according to input AVStream）
 		AVStream *in_stream = ifmt_ctx->streams[i];
-		AVStream *out_stream =
-			avformat_new_stream(ofmt_ctx, in_stream->codec->codec);
+		AVStream *out_stream = avformat_new_stream(ofmt_ctx, in_stream->codec->codec);
 		if (!out_stream) {
 			printf("Failed allocating output stream\n");
 			ret = AVERROR_UNKNOWN;
 			goto end;
 		}
+
 		//复制AVCodecContext的设置（Copy the settings of AVCodecContext）
 		ret = avcodec_copy_context(out_stream->codec, in_stream->codec);
 		if (ret < 0) {
-			printf
-				("Failed to copy context from input to output stream codec context\n");
+			printf("Failed to copy context from input to output stream codec context\n");
 			goto end;
 		}
+
 		out_stream->codec->codec_tag = 0;
 		if (ofmt_ctx->oformat->flags & AVFMT_GLOBALHEADER)
 			out_stream->codec->flags |= CODEC_FLAG_GLOBAL_HEADER;
 	}
+
 	//Dump Format------------------
 	av_dump_format(ofmt_ctx, 0, out_filename, 1);
 	//打开输出URL（Open output URL）
@@ -103,6 +105,7 @@ int main(int argc, char *argv[])
 			goto end;
 		}
 	}
+
 	//写文件头（Write file header）
 	ret = avformat_write_header(ofmt_ctx, NULL);
 	if (ret < 0) {
@@ -111,57 +114,55 @@ int main(int argc, char *argv[])
 	}
 
 	start_time = av_gettime();
+
 	while (1) {
 		AVStream *in_stream, *out_stream;
 		//获取一个AVPacket（Get an AVPacket）
 		ret = av_read_frame(ifmt_ctx, &pkt);
-		if (ret < 0)
+		if (ret < 0) {
 			break;
+		}
+
 		//FIX：No PTS (Example: Raw H.264)
 		//Simple Write PTS
 		if (pkt.pts == AV_NOPTS_VALUE) {
 			//Write PTS
 			AVRational time_base1 = ifmt_ctx->streams[videoindex]->time_base;
 			//Duration between 2 frames (us)
-			int64_t calc_duration =
-				(double)AV_TIME_BASE /
+			int64_t calc_duration = (double)AV_TIME_BASE /
 				av_q2d(ifmt_ctx->streams[videoindex]->r_frame_rate);
 			//Parameters
-			pkt.pts =
-				(double)(frame_index * calc_duration) /
+			pkt.pts = (double)(frame_index * calc_duration) /
 				(double)(av_q2d(time_base1) * AV_TIME_BASE);
 			pkt.dts = pkt.pts;
-			pkt.duration =
-				(double)calc_duration / (double)(av_q2d(time_base1) *
+			pkt.duration = (double)calc_duration / (double)(av_q2d(time_base1) *
 												 AV_TIME_BASE);
 		}
+
 		//Important:Delay
 		if (pkt.stream_index == videoindex) {
 			AVRational time_base = ifmt_ctx->streams[videoindex]->time_base;
 			AVRational time_base_q = { 1, AV_TIME_BASE };
 			int64_t pts_time = av_rescale_q(pkt.dts, time_base, time_base_q);
 			int64_t now_time = av_gettime() - start_time;
-			if (pts_time > now_time)
+			if (pts_time > now_time) {
 				av_usleep(pts_time - now_time);
-
+			}
 		}
 
 		in_stream = ifmt_ctx->streams[pkt.stream_index];
 		out_stream = ofmt_ctx->streams[pkt.stream_index];
 		/* copy packet */
 		//转换PTS/DTS（Convert PTS/DTS）
-		pkt.pts =
-			av_rescale_q_rnd(pkt.pts, in_stream->time_base,
+		pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base,
 							 out_stream->time_base,
 							 (AVRounding) (AV_ROUND_NEAR_INF |
 										   AV_ROUND_PASS_MINMAX));
-		pkt.dts =
-			av_rescale_q_rnd(pkt.dts, in_stream->time_base,
+		pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->time_base,
 							 out_stream->time_base,
 							 (AVRounding) (AV_ROUND_NEAR_INF |
 										   AV_ROUND_PASS_MINMAX));
-		pkt.duration =
-			av_rescale_q(pkt.duration, in_stream->time_base,
+		pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base,
 						 out_stream->time_base);
 		pkt.pos = -1;
 		//Print to Screen
@@ -169,9 +170,9 @@ int main(int argc, char *argv[])
 			printf("Send %8d video frames to output URL\n", frame_index);
 			frame_index++;
 		}
+
 		//ret = av_write_frame(ofmt_ctx, &pkt);
 		ret = av_interleaved_write_frame(ofmt_ctx, &pkt);
-
 		if (ret < 0) {
 			printf("Error muxing packet\n");
 			break;
@@ -199,6 +200,6 @@ end:
 	return 0;
 }
 
-// g++ ffmpeg_stream.c -o ffmpeg_stream -I /usr/local/include -L /usr/local/lib -lavformat -lavcodec -lavutil
+// g++ ffmpeg_stream.c -o ffmpeg_stream -I /usr/local/include -L /usr/local/lib -lavformat -lavcodec -lavutil -lswresample -llzma -lX11 -lvdpau -lpthread -ldl -lm -lz -lrt
 
 
