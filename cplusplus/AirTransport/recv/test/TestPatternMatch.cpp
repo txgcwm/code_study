13072a9a194bed9ee95d93d2b164fee6cdac7b24
wm8996 <txgcwm@163.com>
2017-03-29 17:53:07 +0800

    update code

--- cplusplus/AirTransport/recv/test/TestPatternMatch.cpp
+++ cplusplus/AirTransport/recv/test/TestPatternMatch.cpp
@@ -1,10 +1,16 @@
 #include <string.h>
 #include <stdio.h>
+#include <algorithm>
 #include <string>
 
 #include "PatternMatch.h"
 
 
+
+bool lessmark(const int& ele1, const int& ele2)
+{
+    return ele1 < ele2;
+}
 
 int main(int argc, char **argv)
 {
@@ -18,17 +24,23 @@ int main(int argc, char **argv)
 	// 				305, 307, 319, 321, 338, 340, 351, 353, 371, 373, 383, 385, 404, 406, 415, 417};
 
 	// 68, 91, 96, 122, 222, 
-	int data[] = {68, 73, 74, 77, 79, 91, 96, 100, 122, 124, 132, 149, 167, 182, 199, 215, 222, 
-					231, 248, 263, 281, 295, 314, 327, 347, 359, 380, 391};
+	// int data[] = {68, 73, 74, 77, 79, 91, 96, 100, 122, 124, 132, 149, 167, 182, 199, 215, 222, 
+	// 				231, 248, 263, 281, 295, 314, 327, 347, 359, 380, 391};
 
 	// int data[] = {71, 72, 73, 74, 75, 77, 79, 91, 100, 124, 132, 149, 167, 182, 199, 215,\
 	// 				231, 248, 263, 281, 295, 314, 327, 347, 359, 380, 391};
+
+	int data[] = {68, 73, 74, 263, 77, 96, 100, 167, 182, 359, 380, 199, 215, 222, 
+					231, 248, 281, 295, 122, 124, 314, 132, 149, 327, 347, 79, 91, 391};
+
 	std::vector<int> vecNum;
 	char pattern[] = {0, 1, 4, 6};
 	CPatternMatch *pmatch = new CPatternMatch(pattern);	
 
 	for(int i = 0; i < sizeof(data)/sizeof(data[0]); i++) {
 		vecNum.push_back(data[i]);
+
+		std::sort(vecNum.begin(), vecNum.end(), lessmark);
 
 		// printf("raw data count: %d\n", i);
 		pmatch->QueryIdentificationPackage(vecNum);
