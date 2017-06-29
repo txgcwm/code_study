bb76815afd971311ad516ae09490912dd22f4c08
wm8996 <txgcwm@163.com>
2017-06-29 20:25:05 +0800

    update

---
+++ cplusplus/gtest-cmake-example/environment/FooEnvironment.h
@@ -0,0 +1,12 @@
+class FooEnvironment : public testing::Environment
+{
+public:
+    virtual void SetUp()
+    {
+        std::cout << "Foo FooEnvironment SetUP" << std::endl;
+    }
+    virtual void TearDown()
+    {
+        std::cout << "Foo FooEnvironment TearDown" << std::endl;
+    }
+};
---
+++ doc/gtest资源集.md
@@ -0,0 +1,3 @@
+[C++单元测试框架：gtest](https://my.oschina.net/vaero/blog/214893)  
+[玩转Google开源C++单元测试框架Google Test系列(gtest)(总)](http://www.cnblogs.com/coderzh/archive/2009/04/06/1426755.html)  
+[轻松编写 C++ 单元测试](https://www.ibm.com/developerworks/cn/linux/l-cn-cppunittest/)  
