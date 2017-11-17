9451abfa2ba7197b22fd5c87d254ab92e2532989
wm8996 <wm8996@closeli.cn>
2017-11-17 13:35:22 +0800

    update

---
+++ c/mbedtls/src/libubox-base64-test.c
@@ -0,0 +1,14 @@
+#include "libubox/utils.h"
+
+
+
+int main(int argc, char** argv)
+{
+    // int b64_encode(const void *src, size_t src_len, void *dest, size_t dest_len);
+    // int b64_decode(const void *src, void *dest, size_t dest_len);
+
+    // #define B64_ENCODE_LEN(_len)	((((_len) + 2) / 3) * 4 + 1)
+    // #define B64_DECODE_LEN(_len)	(((_len) / 4) * 3 + 1)
+
+    return 0;
+}
