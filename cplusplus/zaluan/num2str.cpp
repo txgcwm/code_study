73f8b62f41ac38b0c34b3eb5664a95581bf3bfed
wm8996 <wm8996@closeli.cn>
2017-11-24 18:33:17 +0800

    update

--- cplusplus/zaluan/num2str.cpp
+++ cplusplus/zaluan/num2str.cpp
@@ -13,9 +13,22 @@ std::string num2str(T num)
     return ss.str();
 }
 
+template<class TO,class TI>
+TO convert(const TI& t)
+{
+    std::stringstream stream;
+    TO result;
+
+    stream<<t;
+    stream>>result;
+
+    return result;
+}
+
 int main(int argc, char** argv)
 {
     printf("%s\n", num2str(45).c_str());
+    printf("%f\n", convert<double>("12.5"));
 
     return 0;
 }
